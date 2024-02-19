f16148576aa9f39fbc5726c8de974dc160834001
menso431 <menson.sundash@gmail.com>
2024-02-19 23:00:54 +1100

    Added Middleware using next

--- src/routes/v1/contact.routes.js
+++ src/routes/v1/contact.routes.js
@@ -1,8 +1,14 @@
 const app = require("express").Router();
 
-app.get( '/', (req,res) => {
-    res.send('GET request Successfull')
-})
+app.get( '/', (req,res, next) => {
+    // middleware
+    console.log(`Request from: ${req.originalUrl}`)
+    console.log(`Request type: ${req.method}`)
+    next();
+    }, (req, res, next) => {
+        res.send('GET request Successfull')
+    }
+)
 
 app.post( '/', (req,res) => {
     res.send('POST request Successfull')
