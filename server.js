9f17fadd52436d763222fed8a9514d2453db98eb
menso431 <menson.sundash@gmail.com>
2024-02-20 21:48:16 +1100

    MongoDB setup with bodyParser to use JSON

--- server.js
+++ server.js
@@ -1,17 +1,30 @@
 const express = require("express");
 const routes = require('./src/routes/index');
-
-const PORT = 3000;
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 //Initialize express
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send(`Node and express server is Running on port ${PORT}.`)
-});
+const PORT = 3000;
+
+//mongoose connection
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://127.0.0.1:27017/myDatabase', { useNewUrlParser: true })
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => console.log(err));
+
+//bodyparser setup
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
 //routes
 app.use('/api', routes);
+
+
+app.get('/', (req, res) => {
+    res.send(`Node and express server is Running on port ${PORT}.`)
+});
 
 //set port and listen for requests
 app.listen(PORT, () => {
